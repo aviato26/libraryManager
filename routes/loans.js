16991efa970e9b11aa07b1790e622211a4287b8f
aviato26 <sdeshler426@aol.com>
2018-09-26 20:14:35 -0700

    added loan page and models

--- routes/loans.js
+++ routes/loans.js
@@ -4,6 +4,9 @@ const Books = require('../models').Books;
 const Loans = require('../models').Loans;
 const Patrons = require('../models').Patrons;
 const parser = require('body-parser');
+let books;
+let loans;
+let totalLoans;
 
 router.get('/all_loans.pug', (req, res) => {
   Books.findAll({
@@ -19,14 +22,31 @@ router.get('/all_loans.pug', (req, res) => {
     ]
   })
   .then(data => {
-    return data.map(c => {
-    if(c.dataValues.Loans.length > 0){
-      c.dataValues.Loans
-      }
-    })
+    return data.filter(c => c.dataValues.Loans.length > 0)
   })
-  .then(c => console.log(c))
-  res.render('../views/all_loans.pug')
+  .then(loan => {
+      books = loan.map(c => { return title= {t: c.dataValues.title}})
+      loans = loan.map(c => {
+            return c.Loans.map(c => {
+              return {
+                firstName: c.dataValues.Patron.dataValues.first_name,
+                lastName: c.dataValues.Patron.dataValues.last_name,
+                loanOn: c.dataValues.loaned_on,
+                returnBy: c.dataValues.return_by,
+                returnOn: c.dataValues.returned_on
+              }
+          })
+        })
+      })
+  .then((filteredLoan) => {
+    totalLoans = {...books};
+    console.log(totalLoans)
+    res.render('../views/all_loans.pug',
+      {
+        filteredLoan
+      }
+    )
+  })
 })
 
 module.exports = router;
